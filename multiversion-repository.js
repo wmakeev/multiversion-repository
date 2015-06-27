b8a163393b1c8f538866560c382f06cd23218dc4
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-27 18:43:15 +0500

    Initial commit

---
+++ multiversion-repository.js
@@ -0,0 +1,12 @@
+define({
+  "dependencies": {
+    "lodash": {
+      "2.4.1": "//cdn.jsdelivr.net/lodash/2.4.1/lodash.min.js",
+      "3.0.8": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js"
+    },
+    "moment": {
+      "2.10.3": "//cdn.jsdelivr.net/momentjs/2.10.3/moment.min.js"
+    }
+  },
+  "format_version": "1"
+});
