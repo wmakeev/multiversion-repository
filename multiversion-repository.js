7fe67aff01f00008d83b38a84e2effd8aef63e9a
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-28 10:15:38 +0500

    fix version

--- multiversion-repository.js
+++ multiversion-repository.js
@@ -2,7 +2,7 @@ define({
   "modules": {
     "lodash": {
       "2.4.1": "//cdn.jsdelivr.net/lodash/2.4.1/lodash.min.js",
-      "3.0.8": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js"
+      "3.8.0": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js"
     },
     "moment": {
       "2.10.3": "//cdn.jsdelivr.net/momentjs/2.10.3/moment.min.js"
