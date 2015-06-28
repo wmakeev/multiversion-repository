2d651f89b007608e3558f15d7b028543d8944306
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-28 13:26:57 +0500

    add lodash 3.9.3

--- multiversion-repository.js
+++ multiversion-repository.js
@@ -2,7 +2,8 @@ define({
   "modules": {
     "lodash": {
       "2.4.1": "//cdn.jsdelivr.net/lodash/2.4.1/lodash.min.js",
-      "3.8.0": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js"
+      "3.8.0": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js",
+      "3.9.3": "https://cdn.rawgit.com/lodash/lodash/3.9.3/lodash.min.js"
     },
     "moment": {
       "2.10.3": "//cdn.jsdelivr.net/momentjs/2.10.3/moment.min.js"
