20681da688a69955a47f6a13826e51b93f8a1a7d
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-29 23:46:44 +0500

    add some libs

--- multiversion-repository.js
+++ multiversion-repository.js
@@ -7,6 +7,24 @@ define({
     },
     "moment": {
       "2.10.3": "//cdn.jsdelivr.net/momentjs/2.10.3/moment.min.js"
+    },
+    "mutation-summary": {
+      "0.0.0": "//wzrd.in/standalone/mutation-summary"
+    },
+    "domjs": {
+      "0.2.3": "//wzrd.in/standalone/domjs@0.2.3"
+    },
+    "stampit": {
+      "1.2.0": "//cdn.rawgit.com/stampit-org/stampit/v1.2.0/dist/stampit.min.js"
+    },
+    "kefir": {
+      "2.7.0": "//cdn.rawgit.com/rpominov/kefir/2.7.0/dist/kefir.min.js"
+    },
+    "simulate": {
+      "0.0.4": "//cdn.rawgit.com/airportyh/simulate.js/v0.0.4/simulate.js"
+    },
+    "moysklad-router": {
+      "0.1.2": "//rawgit.com/wmakeev/moysklad-router/master/dist/moysklad-router.js"
     }
   },
   "format_version": "2"
